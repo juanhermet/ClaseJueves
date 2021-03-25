2acaaffb3f101306aa4c16dc346f5b91addca2bc
Joel Funes <yoelk-po@live.com>
2021-03-25 19:17:48 -0300

    Joel Funes

---
+++ LeojSenuf/LeojSenuf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeojSenuf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
