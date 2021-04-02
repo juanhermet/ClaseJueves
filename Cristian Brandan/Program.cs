71f81864c354243a752ebcd8a1ad7ff82b3fc564
Cristian Brandan <cristian_brandan@hotmail.com>
2021-04-02 01:59:26 -0300

    Ingreso IMC

--- Cristian Brandan/Program.cs
+++ Cristian Brandan/Program.cs
@@ -6,7 +6,39 @@ namespace Cristian_Brandan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float peso;
+            float altura;
+
+            Console.WriteLine("Ingrese su peso: ");
+            peso = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su altura: ");
+            altura = float.Parse(Console.ReadLine());
+
+            float imc = peso / (altura * altura);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Bajo peso");
+            }
+            else
+            {
+                if (imc < 24.9)
+                {
+                    Console.WriteLine("Saludable");
+                }
+                else
+                {
+                    if (imc < 29.9)
+                    {
+                        Console.WriteLine("Sobre Peso");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Obesidad");
+                    }
+                }
+            }
         }
     }
 }
